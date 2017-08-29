4ef6ae291ce5ef461cde24d97ddb1714daf99ca4
sciion <martin.estgren@gmail.com>
2017-08-29 15:44:26 +0200

    removed attempted travis integration and fixed splash screen

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # HaxeAI
 Project repo for the course TNM095
+
+## Install
+
+```
+haxelib install flixel
+haxelib install flixel-tools
+haxelib run lime setup
+```
+
+## Run
+
+Replace `neko` with your target of choice.
+
+```
+lime test neko
+```
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,6 +7,9 @@ class PlayState extends FlxState
 	override public function create():Void
 	{
 		super.create();
+		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
+		text.screenCenter();
+		add(text);
 	}
 
 	override public function update(elapsed:Float):Void
